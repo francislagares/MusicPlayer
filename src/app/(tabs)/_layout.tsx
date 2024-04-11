09d4551a95107ca0ef3fc7b7ac00686ab67aa2b8
Francis Lagares <francis.lagares@gmail.com>
2024-04-11 14:06:44 +0200

    feat: add tab icons

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,3 +1,9 @@
+import {
+  FontAwesome,
+  FontAwesome6,
+  Ionicons,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
 import { Tabs } from 'expo-router';
 import { StyleSheet } from 'react-native';
@@ -33,10 +39,46 @@ const TabsNavigation = () => {
         ),
       }}
     >
-      <Tabs.Screen name='favorites' />
-      <Tabs.Screen name='playlists' />
-      <Tabs.Screen name='(songs)' />
-      <Tabs.Screen name='artists' />
+      <Tabs.Screen
+        name='favorites'
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name='heart' size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='playlists'
+        options={{
+          title: 'Playlists',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name='playlist-play'
+              size={20}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='(songs)'
+        options={{
+          title: 'Songs',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='musical-notes-sharp' size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='artists'
+        options={{
+          title: 'Songs',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name='users-line' size={20} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
