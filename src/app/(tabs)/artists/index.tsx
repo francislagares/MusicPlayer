0797fb2147f60b4e4500fc2afa2e4418baa698ad
Francis Lagares <francis.lagares@gmail.com>
2024-04-11 13:35:04 +0200

    feat: create tabs navigation

---
+++ src/app/(tabs)/artists/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import { defaultStyles } from '@/styles';
+
+export const ArtistsScreen = () => {
+  return (
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.text}>Artists Screen</Text>
+    </View>
+  );
+};
+
+export default ArtistsScreen;
